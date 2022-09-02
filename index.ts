a42ec9fc1db0a7eac1bd6844b0ef9185b186bb49
Qreepex <65496825+Qreepex@users.noreply.github.com>
2022-09-02 19:43:56 +0200

    build(deps): bump mongoose from 6.3.5 to 6.5.4

--- index.ts
+++ index.ts
@@ -7,3 +7,49 @@ import RedisHandler from "./src/RedisHandler";
 import Logger from "./utils/Logger";
 
 export { DatabaseHandler, MongoHandler, RedisHandler, channel, guild, user, Logger };
+
+export interface DatabaseHandlerConfig {
+  redis: RedisHandlerConfig;
+  mongo: MongoHandlerConfig;
+}
+
+export interface MongoHandlerConfig {
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  uri: string;
+}
+
+export interface RedisHandlerConfig {
+  host: string;
+  port: number;
+  password: string;
+}
+
+export interface UserSettings {
+  id: string;
+  registered: number;
+  language: string;
+}
+
+export interface ChannelSettings {
+  id: string;
+  guild: string;
+  registered: number;
+  limit: number;
+  mode: number;
+  ignore: Array<string>;
+  filters: Array<number>;
+  regex: null | RegExp;
+  filterUsage: string;
+}
+
+export interface GuildSettings {
+  id: string;
+  prefix: string;
+  registered: number;
+  premium: boolean;
+  adminroles: Array<string>;
+  modroles: Array<string>;
+}
